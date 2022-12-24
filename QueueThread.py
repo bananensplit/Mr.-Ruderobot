9cfd67641c921cfa07188d5a8e5b991fa813542c
Jeremiasz <jeremiasz.zrolka@gmail.com>
2022-12-24 13:39:02 +0100

    implemented a working version with asyncio

--- QueueThread.py
+++ QueueThread.py
@@ -8,19 +8,18 @@ from datetime import datetime
 
 
 class QueueThread(threading.Thread):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.queue = Queue()
         self.logger = logging.getLogger("KURWA")
+        self.timer = 0
 
-    def put(self, question, callback):
-        self.logger.debug("Put question in queue: %s", question)
-        self.queue.put([question, callback])
+    def put(self, question):
+        self.queue.put(question)
+        self.timer = 0
 
     def get_pending_requests(self) -> int:
         return self.queue.qsize()
-
 
     def run(self) -> None:
         while True:
@@ -28,39 +27,35 @@ class QueueThread(threading.Thread):
                 time.sleep(1)
                 continue
 
-            self.logger.debug("Detected content in queue. Processing...")
-
-            question, callback = self.queue.get()
-            self.logger.debug("Content: %s", question)
-            self.logger.debug("Callback: %s", callback)
-
-            # Do request    
-            openai.api_key = "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"
-
-            prompt = "The following is a conversation with an AI assistant named 'Mr. Robot'. The assistant will answer every question in a sarcastic, insulting and snooty manner. The AI assistant will occasionally also use words like 'idiot' and 'smartass'.\n\nHuman:"
-            prompt += question
-            prompt += "\nAI: "
-
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt,
-                temperature=0.7,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=["\nAI:", "\nHuman:"],
-            )
-
-            answer = response["choices"][0]["text"].strip()
-
-            self.logger.debug("Answer to question: %s", answer)
-
-            with MongoClient("mongodb://localhost:27017/") as client:
-                collection = client["chat-gpt"]["requests"]
-                collection.insert_one({"question": question, "answer": answer, "response": response, "time": datetime.now()})
-
-            callback(answer)
-            self.queue.task_done()
+            question = self.queue.get()
+            answer = ask_openapi(question)
 
             time.sleep(5)
+
+
+def ask_openapi(question):
+    # Do request
+    openai.api_key = "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"
+
+    prompt = "The following is a conversation with an AI assistant named 'Mr. Robot'. The assistant will answer every question in a sarcastic, insulting and snooty manner. The AI assistant will occasionally also use words like 'idiot' and 'smartass'.\n\nHuman:"
+    prompt += question
+    prompt += "\nAI: "
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\nAI:", "\nHuman:"],
+    )
+
+    answer = response["choices"][0]["text"].strip()
+
+    with MongoClient("mongodb://localhost:27017/") as client:
+        collection = client["chat-gpt"]["requests"]
+        collection.insert_one({"question": question, "answer": answer, "response": response, "time": datetime.now()})
+    
+    return answer
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+import asyncio
+import logging
+import threading
+import openai_async
+from concurrent.futures import Future
 from datetime import datetime
 from pprint import pprint
-from queue import Queue
-import openai
-import threading
-import asyncio
-import concurrent
-import logging
-from pymongo import MongoClient
-from QueueThread import QueueThread
 
+import openai
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
+from pymongo import MongoClient
 
 # Logging
 logger = logging.getLogger("KURWA")
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-# ch.setLevel(logging.INFO)
 formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+loop = asyncio.new_event_loop()
 
-queue_thread = QueueThread(name="QueueThread")
+def create_queue_thread(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+queue_thread = threading.Thread(target=create_queue_thread, args=(loop,))
 queue_thread.start()
+lock = asyncio.Lock(loop=loop)
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -47,6 +52,10 @@ def get_total_requests():
         total_requests = collection.count_documents({})
     return total_requests
 
+def get_pending_requests():
+    global loop
+    return len(asyncio.all_tasks(loop=loop))
+
 
 @app.on_event("startup")
 async def startup_event():
@@ -54,24 +63,19 @@ async def startup_event():
 
 
 @app.on_event("shutdown")
-def shutdown_event():
+async def shutdown_event():
     logger.info("FastAPI shutdown")
 
 
 @app.get("/api/metadata", status_code=200)
 async def get_metadata(response: Response):
-    global queue_thread
-    # data = asyncio.all_tasks(loop)
-    # print("Name                              done          loop")
-    # for task in data:
-        # print(f"{task.get_name(): <34s}{str(task.done()): <14s}{task._loop}")
-    # print()
-    return {"totalrequests": get_total_requests(), "pendingrequests": queue_thread.get_pending_requests()}
+    total, pending = get_total_requests(), get_pending_requests()
+    logger.debug("Received metadata request: totalrequests: %s, pendingrequests: %s", total, pending)
+    return {"totalrequests": total, "pendingrequests": pending}
 
 
 @app.post("/api/askquestion", status_code=200)
 async def askquestion(request: QuestionModel, response: Response):
-    global queue_thread    
     question = request.question
     logger.debug("Received question: %s", question)
 
@@ -79,18 +83,72 @@ async def askquestion(request: QuestionModel, response: Response):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": "Question must be between 1 and 128 characters long."}
 
+    logger.debug("Processing question: %s", question)
 
-    answer = "alksjd"
-    def loop_callback(x):
-        answer = x
-        # print("telletubbie: " + answer)
-        # return {"answer": answer}
-
-    logger.debug("Putting question in queue: %s", question)
-    queue_thread.put(question, loop_callback)
-
-    while answer is None or answer == "alksjd":
-        logger.debug("Waiting for answer: %s", answer)
-        await asyncio.sleep(2)
+    future = Future()
+    logger.debug("Created future: %s", question)
+    loop.call_soon_threadsafe(loop.create_task, ask_openai(question, future))
+    # future.set_result("KURWA")
+    # asyncio.run_coroutine_threadsafe(ask_openapi(question, future), loop)
+    logger.debug("Waiting for result: %s", question)
+    answer = future.result()
+    logger.debug("Got answer: %s (%s)", answer, question)
 
     return {"answer": answer}
+
+
+async def ask_openai(question, future: Future = None):
+    logger.debug("Waiting for lock: %s", question)
+    await lock.acquire()
+    logger.debug("Acquired lock: %s", question)
+
+    # Do request
+    openai.api_key = "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"
+
+    prompt = "The following is a conversation with an AI assistant named 'Mr. Robot'. The assistant will answer every question in a sarcastic, insulting and snooty manner. The AI assistant will occasionally also use words like 'idiot' and 'smartass'.\n\nHuman:"
+    prompt += question
+    prompt += "\nAI: "
+
+    response = await openai_async.complete(
+        "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",
+        timeout=None,
+        payload={
+            "model": "text-davinci-003",
+            "prompt": prompt,
+            "temperature": 0.7,
+            "max_tokens": 256,
+            "top_p":1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": ["\nAI:", "\nHuman:"],
+        }
+    )
+    response = response.json()
+
+    #TODO: Schauen wann die acreate commiten und dann die Version hier verwenden
+    # response = openai.Completion.acreate(
+    #     model="text-davinci-003",
+    #     prompt=prompt,
+    #     temperature=0.7,
+    #     max_tokens=256,
+    #     top_p=1,
+    #     frequency_penalty=0,
+    #     presence_penalty=0,
+    #     stop=["\nAI:", "\nHuman:"],
+    # )
+
+    answer = response["choices"][0]["text"].strip()
+
+    with MongoClient("mongodb://localhost:27017/") as client:
+        collection = client["chat-gpt"]["requests"]
+        collection.insert_one({"question": question, "answer": answer, "response": response, "time": datetime.now()})
+    
+    # Setting future result
+    logger.debug("Setting future: %s", question)
+    future.set_result(answer)
+
+    logger.debug("Sleeping for 5 more seconds: %s", question)
+    await asyncio.sleep(5)
+
+    logger.debug("Releasing lock: %s", question)
+    lock.release()
